b55afeb03601849c7c9d0241ecd85513efdff501
c4710n <c4710n@users.noreply.github.com>
2023-01-10 21:12:50 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyCase.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Converts data between common naming conventions, such as snake case, kebab case, camel case and pascal case."
   @source_url "https://github.com/cozy-elixir/cozy_case"
 
