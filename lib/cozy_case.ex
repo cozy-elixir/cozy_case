f6f9ef1fbe538bd271320b52bb8e7d0d8374e03f
c4710n <c4710n@users.noreply.github.com>
2023-01-10 20:07:43 +0800

    feat: fill `CozyCase`

--- lib/cozy_case.ex
+++ lib/cozy_case.ex
@@ -1,18 +1,118 @@
 defmodule CozyCase do
   @moduledoc """
-  Documentation for `CozyCase`.
-  """
+  Convert data between common naming conventions, such as snake case, kebab case, camel case and
+  pascal case.
 
-  @doc """
-  Hello world.
+  Currently, this module provides 4 main functions:
+
+  + `snake_case/1`
+  + `kebab_case/1`
+  + `camel_case/1`
+  + `pascal_case/1`
 
   ## Examples
 
-      iex> CozyCase.hello()
-      :world
+  All these 4 functions have the same intefaces.
+
+  For strings or atoms, these functions convert them directly:
+
+      iex> CozyCase.snake_case("HelloWorld")
+      "hello_world"
+
+      iex> CozyCase.snake_case(HelloWorld)
+      "hello_world"
+
+  For maps, there functions convert the keys of maps recursively, without touching the values
+  of maps:
+
+      iex> CozyCase.snake_case(%{
+      iex>   "FamilyMembers" => [
+      iex>     %{
+      iex>       "Name" => "Lily",
+      iex>       "Age" => 50,
+      iex>       "Hobbies" => ["Dreaming", "Singing"]
+      iex>     },
+      iex>     %{
+      iex>       "Name" => "Charlie",
+      iex>       "Age" => 55,
+      iex>       "Hobbies" => ["Dreaming", "Singing"]
+      iex>     }
+      iex>   ]
+      iex> })
+      %{
+        "family_members" => [
+          %{"name" => "Lily", "age" => 50, "hobbies" => ["Dreaming", "Singing"]},
+          %{"name" => "Charlie", "age" => 55, "hobbies" => ["Dreaming", "Singing"]}
+        ]
+      }
+
+  For lists, these functions convert the keys of maps in lists recursively:
+
+      iex> CozyCase.snake_case([
+      iex>     %{
+      iex>       "Name" => "Lily",
+      iex>       "Age" => 50,
+      iex>       "Hobbies" => ["Dreaming", "Singing"]
+      iex>     },
+      iex>     %{
+      iex>       "Name" => "Charlie",
+      iex>       "Age" => 55,
+      iex>       "Hobbies" => ["Dreaming", "Singing"]
+      iex>     }
+      iex>   ])
+      [
+        %{"name" => "Lily", "age" => 50, "hobbies" => ["Dreaming", "Singing"]},
+        %{"name" => "Charlie", "age" => 55, "hobbies" => ["Dreaming", "Singing"]}
+      ]
 
   """
-  def hello do
-    :world
+
+  alias CozyCase.SnakeCase
+  alias CozyCase.KebabCase
+  alias CozyCase.CamelCase
+  alias CozyCase.PascalCase
+
+  def snake_case(term) when is_binary(term) or is_atom(term), do: convert_plain(term, &SnakeCase.convert/1)
+  def snake_case(term) when is_map(term) or is_list(term), do: convert_nest(term, &SnakeCase.convert/1)
+
+  def kebab_case(term) when is_binary(term) or is_atom(term), do: convert_plain(term, &KebabCase.convert/1)
+  def kebab_case(term) when is_map(term) or is_list(term), do: convert_nest(term, &KebabCase.convert/1)
+
+  def camel_case(term) when is_binary(term) or is_atom(term), do: convert_plain(term, &CamelCase.convert/1)
+  def camel_case(term) when is_map(term) or is_list(term), do: convert_nest(term, &CamelCase.convert/1)
+
+  def pascal_case(term) when is_binary(term) or is_atom(term), do: convert_plain(term, &PascalCase.convert/1)
+  def pascal_case(term) when is_map(term) or is_list(term), do: convert_nest(term, &PascalCase.convert/1)
+
+  defp convert_plain(string, fun) when is_binary(string) do
+    fun.(string)
   end
+
+  defp convert_plain(atom, fun) when is_atom(atom) do
+    Atom.to_string(atom)
+    |> case do
+      "Elixir." <> rest -> rest
+      string -> string
+    end
+    |> fun.()
+  end
+
+  defp convert_plain(any, _fun), do: any
+
+  defp convert_nest(map, fun) when is_map(map) do
+    try do
+      for {k, v} <- map,
+          into: %{},
+          do: {convert_plain(k, fun), convert_nest(v, fun)}
+    rescue
+      # not Enumerable
+      Protocol.UndefinedError -> map
+    end
+  end
+
+  defp convert_nest(list, fun) when is_list(list) do
+    Enum.map(list, &convert_nest(&1, fun))
+  end
+
+  defp convert_nest(any, _fun), do: any
 end
--- test/cozy_case_test.exs
+++ test/cozy_case_test.exs
@@ -1,8 +1,64 @@
 defmodule CozyCaseTest do
   use ExUnit.Case
+
   doctest CozyCase
 
-  test "greets the world" do
-    assert CozyCase.hello() == :world
+  describe "snake_case/1" do
+    test "works for strings" do
+      assert "welcome_message" = CozyCase.snake_case("WelcomeMessage")
+    end
+
+    test "works for atoms" do
+      assert "welcome_message" = CozyCase.snake_case(:WelcomeMessage)
+    end
+
+    test "works for maps and lists" do
+      assert %{
+               "name" => "Lenna",
+               "age" => 23,
+               "hobbies" => ["Fishing", "Singing"],
+               "family_members" => %{
+                 "father" => %{
+                   "name" => "Charlie",
+                   "age" => 55,
+                   "hobbies" => ["Hacking", "Singing"]
+                 },
+                 "mother" => %{
+                   "name" => "Lily",
+                   "age" => 50,
+                   "hobbies" => ["Dreaming", "Singing"]
+                 }
+               }
+             } ==
+               CozyCase.snake_case(%{
+                 "Name" => "Lenna",
+                 "Age" => 23,
+                 "Hobbies" => ["Fishing", "Singing"],
+                 "FamilyMembers" => %{
+                   mother: %{
+                     "Name" => "Lily",
+                     "Age" => 50,
+                     "Hobbies" => ["Dreaming", "Singing"]
+                   },
+                   father: %{
+                     "Name" => "Charlie",
+                     "Age" => 55,
+                     "Hobbies" => ["Hacking", "Singing"]
+                   }
+                 }
+               })
+    end
+  end
+
+  describe "kebab_case/1" do
+    # NO NEED TO TEST
+  end
+
+  describe "camel_case/1" do
+    # NO NEED TO TEST
+  end
+
+  describe "pascal_case/1" do
+    # NO NEED TO TEST
   end
 end
